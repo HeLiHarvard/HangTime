4ff52682d351a6623176fca4a16d736100f8de56
He Li <heli@college.harvard.edu>
2014-02-12 23:25:53 -0500

    created first user with parse

--- HangTime1/HangTime1ViewController.m
+++ HangTime1/HangTime1ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "HangTime1ViewController.h"
+#import <Parse/Parse.h>
 
 @interface HangTime1ViewController ()
 
@@ -18,6 +19,23 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    PFUser *user = [PFUser user];
+    user.username = @"my name";
+    user.password = @"my pass";
+    user.email = @"email@example.com";
+    
+    // other fields can be set if you want to save more information
+    user[@"phone"] = @"650-555-0000";
+    
+    [user signUpInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            // Hooray! Let them use the app now.
+        } else {
+            NSString *errorString = [error userInfo][@"error"];
+            // Show the errorString somewhere and let the user try again.
+        }
+    }];
+    
 }
 
 - (void)didReceiveMemoryWarning
